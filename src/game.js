53f81323d8bf7d60007e11b1f11896e64c679c6f
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-07-06 21:26:01 -0400

    Complete game feature/functionality

--- src/game.js
+++ src/game.js
@@ -1,7 +1,9 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-// const {createCard, } = require('./card'); 
+const {createCard, } = require('./card'); 
+const { createDeck, countCards } = require('../src/deck');
+const { createRound, takeTurn } = require('../src/round');
 
 function printMessage(deck) {
   console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
@@ -12,4 +14,19 @@ function printQuestion(round) {
   util.main(round);
 }
 
-module.exports = { printMessage, printQuestion };
+function start() {
+  const cards = prototypeQuestions.map((card) => {
+    return createCard(card.id, card.question, card.answers, card.correctAnswer)
+  })
+  const deck = createDeck(cards)
+  const round1 = createRound(deck)
+
+  printMessage(deck)
+  printQuestion(round1)
+}
+
+module.exports = { 
+  printMessage, 
+  printQuestion,
+  start
+};
